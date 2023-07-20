15aa7bcb0c3e1cb758f9c3f1fa660a825c836af7
jitendra.kumar1@dotsquares.com-at-685174754825 <jitendra.kumar1@dotsquares.com>
2023-07-20 19:04:41 +0530

    slider

--- src/product.js
+++ src/product.js
@@ -1,25 +1,79 @@
 import React from "react";
 import products from "./data";
+import { Modal, Button } from 'react-bootstrap'
+import { useState } from "react";
+import Slider from 'slider-moon';
+import 'slider-moon/dist/style.css'
 
 const Product = () => {
-    console.log(products);
+    const [isShow, setInvokeModal] = useState("")
+    const initModal = () => {
+        return setInvokeModal("")
+    }
+
+    const ModalDialog = (key) => {
+        console.log(key);
+        setInvokeModal(key)
+    }
     return (
         <>
             <div className="row">
-                {products.map((item,key) => (
-                    <div key={key} className="col-3 product" style={{"border": "1px solid lightgray","border-radius": "5px","padding": "5px","margin": "5px","text-align":"center"}}>
-                        <p>{item.brand}</p>
-                        <h6>{item.title}</h6>
-                        <img src={item.thumbnail} width={200} height={200} />
-                        <p style={{"font-weight": "bold"}}>Price : {item.price}</p>
-                        <p style={{"color": "red"}}>Discount : {item.discountPercentage} %</p>
-                        <p style={{"text-align": "justify"}}>{item.description.substring(0,50)}...</p>
-                    </div>
+                {products.map((item, key) => (
+                    <>
+                        <div onClick={() => ModalDialog(key)} key={key} className="col-3 product" style={{ "border": "1px solid lightgray", "borderRadius": "5px", "padding": "5px", "margin": "5px", "textAlign": "center", "cursor": "pointer" }}>
+                            <p>{item.brand}</p>
+                            <h6>{item.title}</h6>
+                            <img src={item.thumbnail} width={200} height={200} />
+                            <p style={{ "fontWeight": "bold" }}>Price : {item.price}</p>
+                            <p style={{ "color": "red" }}>Discount : {item.discountPercentage} %</p>
+                            <p style={{ "textAlign": "justify" }}>{item.description.substring(0, 50)}...</p>
+
+                        </div>
+                        <Modal key={`model-${key}`} show={(key === isShow) ? true : false}>
+                            <Modal.Header onClick={() => initModal()}>
+                                <Modal.Title> {item.title} ( {item.brand} )</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+                                <Slider
+                                    style={{ "width": "200px" }}
+                                    slideClass={'my-slider-grid'}
+                                    infinite={true}
+                                    bullets={true}
+                                    arrowsNav={true}
+                                    animation={'scale'}
+                                    callback={() => {
+                                        console.log('here');
+                                    }}
+                                >
+                                    <div className='slider my-slider-grid'>
+                                        <ul className='slider-wrapper'>
+                                            {item.images.map((imgPath, imgKey) => (
+                                                <li key={imgKey} style={{ "textAlign": "center" }}>
+                                                    <img src={imgPath} width={200} height={200} />
+                                                </li>
+                                            ))}
+                                        </ul>
+                                    </div>
+                                </Slider>
+                                {/* <img src={item.thumbnail} width={200} height={200} /> */}
+                                <p style={{ "fontWeight": "bold" }}>Price : {item.price}</p>
+                                <p style={{ "color": "red" }}>Discount : {item.discountPercentage} %</p>
+                                <p style={{ "textAlign": "justify" }}>{item.description}</p>
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <Button variant="danger" onClick={() => initModal()}>
+                                    Close
+                                </Button>
+                            </Modal.Footer>
+                        </Modal>
+
+                    </>
                 ))}
             </div>
         </>
     )
 }
+
 export {
     Product
 }
--- src/sidebar.js
+++ src/sidebar.js
@@ -2,13 +2,13 @@ import React from "react";
 const Sidebar = () => {
     return (
         <>
-            <ul className="navbar-nav" style={{ "border-right": "1px solid lightgray", "text-align": "center" }}>
-                <li className="nav-item active" style={{ "border-bottom": "1px solid lightgray", "padding": "5px", "margin": "5px" }}>
+            <ul className="navbar-nav" style={{ "borderRight": "1px solid lightgray", "textAlign": "center" }}>
+                <li className="nav-item active" style={{ "borderBottom": "1px solid lightgray", "padding": "5px", "margin": "5px" }}>
                     <a className="nav-link" href="#">
                         Dashborad <span className="sr-only">(current)</span>
                     </a>
                 </li>
-                <li className="nav-item" style={{ "border-bottom": "1px solid lightgray", "padding": "5px", "margin": "5px" }}>
+                <li className="nav-item" style={{ "borderBottom": "1px solid lightgray", "padding": "5px", "margin": "5px" }}>
                     <a className="nav-link" href="#">
                         Products
                     </a>
